572f4a3f4c0f5a8a88445883f05c009a8b95e832
joeryan <joe@clanryan.us>
2014-01-09 20:51:28 -0500

    update marker_spec.rb with additional contexts

--- spec/codebreaker/marker_spec.rb
+++ spec/codebreaker/marker_spec.rb
@@ -2,32 +2,46 @@ require 'spec_helper'
 
 module Codebreaker
   describe Marker do
-    describe "#exact_match_count" do
-      context "with no matches" do
+    describe '#exact_match_count' do
+      context "with no match" do
         it "returns 0" do
-          marker = Marker.new('rbyc','gggg')
-          marker.exact_match_count.should ==0
+          marker = Marker.new('rbyc', 'gggg')
+          marker.exact_match_count.should == 0
         end
       end
 
       context "with 1 exact match" do
         it "returns 1" do
-          marker = Marker.new('rbyc','rggg')
-          marker.exact_match_count.should ==1
+          marker = Marker.new('rbyc', 'rggg')
+          marker.exact_match_count.should == 1
         end
       end
 
       context "with 1 color match" do
         it "returns 0" do
-          marker = Marker.new('rbyc','bggg')
-          marker.exact_match_count.should ==0
+          marker = Marker.new('rbyc', 'grgg')
+          marker.exact_match_count.should == 0
         end
       end
 
-      context "with 1 exact match and 1 number match" do
+      context "with 1 exact matche and 1 color match" do
         it "returns 1" do
-          marker = Marker.new('rbyc','rgbg')
-          marker.exact_match_count.should ==1
+          marker = Marker.new('rbyc', 'rgbg')
+          marker.exact_match_count.should == 1
+        end
+      end
+
+      context "with 3 exact matches" do
+        it "returns 3" do
+          marker = Marker.new('rbyc', 'rbyg')
+          marker.exact_match_count.should == 3
+        end
+      end
+
+      context "with 4 exact matches" do
+        it "returns 4" do
+          marker = Marker.new('rbyc', 'rbyc')
+          marker.exact_match_count.should == 4
         end
       end
     end
@@ -35,29 +49,29 @@ module Codebreaker
     describe "#color_match_count" do
       context "with no matches" do
         it "returns 0" do
-          marker = Marker.new('rbyc','gggg')
-          marker.color_match_count.should ==0
-        end
-      end
-      
-      context "with 1 color match" do
-        it "returns 1" do
-          marker = Marker.new('rbyc','bggg')
-          marker.color_match_count.should ==1
+          marker = Marker.new('rbyc', 'gggg')
+          marker.color_match_count.should == 0
         end
       end
 
-      context "with 1 exact match" do
-        it "returns 0" do
-          marker = Marker.new('rbyc','rggg')
-          marker.color_match_count.should ==0
+      context "with 1 color match" do
+        it "returns 1" do
+          marker = Marker.new('rbyc', 'grgg')
+          marker.color_match_count.should == 1
         end
       end
-  
+      
+      context "with 1 exact match" do
+        it "returns 0" do
+          marker = Marker.new('rbyc', 'rggg')
+          marker.color_match_count.should == 0
+        end
+      end
+
       context "with 1 exact match and 1 color match" do
         it "returns 1" do
-          marker = Marker.new('rbyc','rygg')
-          marker.color_match_count.should ==1
+          marker = Marker.new('rbyc', 'rgbg')
+          marker.color_match_count.should == 1
         end
       end
     end
