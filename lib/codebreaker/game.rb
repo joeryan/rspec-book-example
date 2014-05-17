10b6df64cfafb8c14debd3de1919883f39a096e5
Joe Ryan <joe@clanryan.us>
2014-05-17 15:04:53 +0430

    add instructions to game

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -8,7 +8,7 @@ module Codebreaker
     def start(secret)
       @secret = secret
       @output.puts 'Welcome to Codebreaker!'
-      @output.puts 'Enter guess (q to quit):'
+      @output.puts 'Enter guess (q to quit or ? for instructions):'
     end
 
     def guess(guess)
@@ -27,5 +27,15 @@ module Codebreaker
       puts "Congratulations, you guessed correctly!"
       exit
     end
+
+    def instructions
+      puts "Type a guess of four colors with no spaces and press Enter."
+      puts "Valid colors are: r - Red, b - Blue, y - Yellow, g - Green, o - Orange"
+      puts "The guess will be evaluated and an indicator provided.  The"
+      puts "indicator will have b for correct colors in the correct spot,"
+      puts "and w - for colors that are correct but in the wrong position."
+      puts "If no colors are correct, a blank line will be output."
+      puts "Enter a guess (q for quit):"
+    end 
   end
 end
